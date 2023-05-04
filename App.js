3cb51bc462d2cb701ea74aab277f14665bde859e
Ubuntu <ubuntu@ip-172-26-14-104.ap-northeast-2.compute.internal>
2023-05-04 09:51:24 +0000

    set initial states

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [input, setInput] = useState(0);
+  const [currentOperator, setCurrentOperator] = useState(null);
+  const [result, setResult] = useState(null);
+  const [tempInput, setTempInput] = useState(null);
+  const [tempOperator, setTempOperator] = useState(null);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
